607497a58ceefd8c98ec2f87c07912a3c8d6181a
Maksim <epic.twod@mail.ru>
2022-06-13 19:22:03 +0300

    Task 3

---
+++ Z3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i<num; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+}
+Console.Write(Math.Pow(num, 3) + ". ");
