7e76fb78f083b87c96aa03858bc95929a4ac50d3
Maksim <epic.twod@mail.ru>
2022-06-13 19:11:38 +0300

    Task 1

---
+++ Z1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<100000 && num>9999)
+{
+    if(num%10 == num/10000 && (num/1000)%10 == (num/10)%10)
+    {
+        Console.Write("Yes");
+    }
+    else
+    {
+        Console.Write("No");
+    }
+}
+else
+{
+    Console.Write("Error. Incorrect number");
+}
