0a8628a11b42d0de70b3f2a343a197ee4eb42fdf
Maksim <epic.twod@mail.ru>
2022-06-13 20:41:08 +0300

    Fixes

--- Z1/Program.cs
+++ Z1/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.Write("Enter your number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if(num<100000 && num>9999)
+﻿void checknum(int num)
 {
+    if(num<100000 && num>9999)
+    {
     if(num%10 == num/10000 && (num/1000)%10 == (num/10)%10)
     {
         Console.Write("Yes");
@@ -10,8 +10,15 @@ if(num<100000 && num>9999)
     {
         Console.Write("No");
     }
-}
+    }
 else
 {
     Console.Write("Error. Incorrect number");
 }
+
+}
+
+Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+checknum(num);
